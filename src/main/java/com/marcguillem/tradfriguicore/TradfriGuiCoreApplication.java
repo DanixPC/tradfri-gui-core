f25b24d025af24f62cc9edf84a90951f2f95ea5a
Marc Guillem <magusan.1970@gmail.com>
2021-06-04 18:43:34 +0200

    SpringBoot initialized

---
+++ src/main/java/com/marcguillem/tradfriguicore/TradfriGuiCoreApplication.java
@@ -0,0 +1,13 @@
+package com.marcguillem.tradfriguicore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TradfriGuiCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TradfriGuiCoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/marcguillem/tradfriguicore/TradfriGuiCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marcguillem.tradfriguicore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TradfriGuiCoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
